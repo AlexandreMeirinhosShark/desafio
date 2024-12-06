0c431d36c879bc0b49f5467ca5f9d209a1e7226a
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-12-06 19:35:14 +0000

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,7 @@
+comida = ["nuggets mc donald's", "pizza", "massa", "nuggets BURGER KING"]
+bebida = ["coca cola", "água", "caldo de cana", "mijo"]
+
+for c in comida:
+    print(c)
+    if c == "nuggets BURGER KING":
+        print("MELHOR DE TODOS CAR###")
